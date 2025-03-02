5387e1f8ebc441b908071754605936e2e8092a51
ZeHansson <chriszehansson@gmail.com>
2025-03-02 15:00:12 +0100

    added markdown to html, and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
+from markdown import split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -41,6 +42,18 @@ class TestTextNode(unittest.TestCase):
     def test_not_eq_ALL(self):
         node = TextNode("This is a FAT text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+
+
+#class Test_Markdown_To_TextNode(unittest.TestCase):
+#    def test_detect_code(self):
+#        self.assertEqual(split_nodes_delimiter([TextNode("This is text with a `code block` word", TextType.TEXT)],"`", TextType.CODE)[1].text,"code block" )
+
+#    def test_detect_bold(self):
+#        self.assertEqual(split_nodes_delimiter([TextNode("This is text with a **bold** word", TextType.TEXT)],"**", TextType.BOLD)[1].text,"bold" )
+
+#    def test_detect_italic(self):
+#        self.assertEqual(split_nodes_delimiter([TextNode("This is text with an _Italic_ word", TextType.TEXT)],"_", TextType.ITALIC)[1].text,"Italic" )
+
 
 if __name__ == "__main__":
     unittest.main()
